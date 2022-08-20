16576fe67895e7995538aeab9f6445fa9cdb8ae3
Alex Gicimo Gakuru <gakurualexgicimo@gmail.com>
2022-08-20 13:54:45 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -11,6 +11,49 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function  titleCased(){
+
+  //Debug:printArray(newTutorials)
+
+  const newTutorials =tutorials.map(capitalizeFirstWord)
+  console.log(newTutorials)
+return newTutorials;
 }
+
+
+
+
+function capitalizeFirstWord(statement){
+
+    let newStatement = statement.split( ` `);
+  //Debug :console.log(`old statement was : "${statement}"`)
+  //Debug :console.log(`new statement is : "${newStatement}"`)
+
+    for (var i = 0; i < newStatement.length; i++) {
+
+        newStatement[i] = newStatement[i].charAt(0).toUpperCase() + newStatement[i].substring(1);
+
+//Debu: Console.log(`New statement after capilization is ${newStatement[i]}`);
+    }
+
+//Debug :console.log(`New statement after title case `+newStatement.join(` `))
+    return newStatement.join(' ');
+ }
+
+
+
+
+/*Debug:
+const printArray=(myArray)=>{
+
+for(let elem of myArray){
+
+  console.log(elem);
+}
+}
+
+
+
+console.log(capitalizeFirstWord(`Alex is Awesome`))
+titleCased();
+*/
